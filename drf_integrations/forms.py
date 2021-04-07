697107ee75a8a68b689ef0ea2329e992b0cd3a81
Diego Milan <diego@yoyowallet.com>
2021-04-06 23:53:38 -0500

    Support installations without integrations

--- drf_integrations/forms.py
+++ drf_integrations/forms.py
@@ -1,7 +1,7 @@
 import copy
 
 from django import forms
-
+from django.contrib.postgres.forms.jsonb import JSONField
 from drf_integrations import models
 from drf_integrations.integrations import Registry
 
@@ -17,6 +17,7 @@ class ApplicationInstallationForm(forms.ModelForm):
         fields = [
             "application",
             models.get_application_installation_install_attribute_name(),
+            "api_client_name",
         ]
 
     def __init__(self, data=None, *args, **kwargs):
@@ -33,17 +34,35 @@ class ApplicationInstallationForm(forms.ModelForm):
             except Registry.IntegrationUnavailableException:
                 pass
 
+        config = self.instance.get_config()
+
+        self.fields['api_client_name'].choices = [('-', '-')] + self.fields['api_client_name'].choices
+
+        if self.initial.get('api_client_name') is None:
+            self.initial['api_client_name'] = '-'
+
         if use_config_class:
-            config = self.instance.get_config()
             integration_class = self.instance.application.get_integration_instance()
             self.integration_form = integration_class.config_form_class
             for name, field in self.integration_form.base_fields.items():
                 self.integration_config_fields.append(name)
                 self.fields[name] = field
                 self.initial[name] = config.get(name, field.initial)
+        else:
+            # Application has no integration
+            # Render config field
+            self.fields['config'] = JSONField()
+            self.initial['config'] = config
 
     def clean(self):
+        # Clean turns '' into None
+        is_default = self.data['api_client_name'] == ''
         data = super().clean()
+
+        if data['api_client_name'] == '-':
+            data['api_client_name'] = None
+        elif is_default:
+            data['api_client_name'] = ''
 
         if self.integration_form:
             data = self.integration_form.clean_form_data(self)
@@ -51,8 +70,13 @@ class ApplicationInstallationForm(forms.ModelForm):
         return data
 
     def save(self, commit=True):
-        self.instance.config = {
-            name: self.cleaned_data.get(name) for name in self.integration_config_fields
-        }
+        if self.integration_form:
+            self.instance.config = {
+                name: self.cleaned_data.get(name) for name in self.integration_config_fields
+            }
+        else:
+            self.instance.config = self.cleaned_data.get('config', {})
 
         return super().save(commit)
+
+
