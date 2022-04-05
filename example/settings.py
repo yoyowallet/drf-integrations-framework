ecfd346c57e6ba22d60504e7163a3a677547e3a8
Stuart Bradley <stuy.bradley@gmail.com>
2022-04-05 14:40:58 +0100

    Fix final cont deploy

--- example/settings.py
+++ example/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("PGDATABASE", "drfintegrationsexample"),
         "USER": os.environ.get("PGUSER", "postgres"),
-        "PASSWORD": os.environ.get("PGPASSWORD", "postgres"),
+        "PASSWORD": os.environ.get("PGPASSWORD"),
         "HOST": os.environ.get("PGHOST", "localhost"),
         "PORT": os.environ.get("PGPORT", 5432),
     }
