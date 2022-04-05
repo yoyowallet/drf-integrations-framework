64415e779ead1fd5de32abfc5ec68deabca61f8a
Stuart Bradley <stuy.bradley@gmail.com>
2022-04-05 13:32:50 +0100

    Fix for envs

--- example/settings.py
+++ example/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("PGDATABASE", "drfintegrationsexample"),
         "USER": os.environ.get("PGUSER", "postgres"),
-        "PASSWORD": os.environ.get("PGPASSWORD"),
+        "PASSWORD": os.environ.get("PGPASSWORD", "postgres"),
         "HOST": os.environ.get("PGHOST", "localhost"),
         "PORT": os.environ.get("PGPORT", 5432),
     }
