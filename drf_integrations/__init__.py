52bbc1d0700186a0e3189da37a24866283d096a8
Del Hyman-Jones <dhymanjones@gmail.com>
2022-06-20 12:13:52 +0100

    export the 4 field functions so that consumers are able to call them if required.

--- drf_integrations/__init__.py
+++ drf_integrations/__init__.py
@@ -1,3 +1,6 @@
+from fields import get_json_model_field, get_json_model_field_import_name  # noqa
+from forms import get_json_form_field, get_json_model_field_import_name  # noqa
+
 VERSION = "0.6.1"
 
 default_app_config = "drf_integrations.apps.DRFIntegrationsConfig"
