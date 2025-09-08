ed54f61266007e697580403a2141e4744ca70421
Del Hyman-Jones <del.hyman-jones@nealstreet.tech>
2025-09-08 10:16:59 +0100

    - GitHub to use postgres 17 - pin setuptools to <81 as recommended by warning messages from tox - remove USE_L10N because it will be deprecated. - fixed GitHub tests actions so that we install the poetry environment for tox and all dependencies are in one place instead of being duplicated under tox.ini in the `deps` section -

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,7 +44,6 @@ def pytest_configure(config):
         SITE_ID=1,
         SECRET_KEY="testing key",
         USE_I18N=True,
-        USE_L10N=True,
         STATIC_URL="/static/",
         ROOT_URLCONF="tests.urls",
         TEMPLATES=[
